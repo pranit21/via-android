2cd853b94353230a09570481fa0e04e09990b693
Pranit <morepranit@gmail.com>
2015-11-15 13:55:12 +0530

    Volley demo updated

--- VolleyDemo/app/src/main/java/com/itvedant/volleydemo/MainActivity.java
+++ VolleyDemo/app/src/main/java/com/itvedant/volleydemo/MainActivity.java
@@ -1,35 +1,51 @@
 package com.itvedant.volleydemo;
 
 import android.app.ProgressDialog;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    ProgressDialog pDialog;
+    private TextView jsonResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        jsonResponse = (TextView) findViewById(R.id.json_response);
+
         // Tag used to cancel the request
         String tag_json_obj = "json_obj_req";
 
-        String url = "json_url";
+        String url = "http://api.androidhive.info/feed/feed.json";
 
-        ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog = new ProgressDialog(this);
         pDialog.setMessage("Loading...");
         pDialog.show();
 
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
                 url, null,
                 new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
                         Log.d(TAG, response.toString());
+                        jsonResponse.setText(response.toString());
                         pDialog.hide();
                     }
                 }, new Response.ErrorListener() {
@@ -42,7 +58,7 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-// Adding request to request queue
+        // Adding request to request queue
         AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
     }
 
